698ea11d9d6653ef522f6e77ef75be277a19601b
Bryan Cordova <bryancordova.hela@gmail.com>
2024-07-01 15:51:29 -0400

    updated data utility

--- BugTrackerBC/Data/DataUtility.cs
+++ BugTrackerBC/Data/DataUtility.cs
@@ -24,22 +24,22 @@ public static class DataUtility
     private static Faker faker = new();
 
 
-    public static string? GetConnectionString(IConfiguration configuration)
+    public static string GetConnectionString(IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
-        return string.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        var connectionString = configuration.GetConnectionString("DefaultConnection");  // Local Connection string
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_PRIVATE_URL");  // Railway connection string
+
+        return string.IsNullOrEmpty(databaseUrl) ? connectionString! : BuildConnectionString(databaseUrl);
     }
 
-    public static string BuildConnectionString(string databaseUrl)
+    private static string BuildConnectionString(string databaseUrl)
     {
-        //Provides an object representation of a uniform resource identifier (URI) and easy access to the parts of the URI.
         var databaseUri = new Uri(databaseUrl);
         var userInfo = databaseUri.UserInfo.Split(':');
 
         var database = Environment.GetEnvironmentVariable("RAILWAY_SERVICE_NAME")
-            ?? typeof(DataUtility).Assembly.GetName().Name; 
-        //Provides a simple way to create and manage the contents of connection strings used by the NpgsqlConnection class.
+            ?? typeof(DataUtility).Assembly.GetName().Name;
+
         var builder = new NpgsqlConnectionStringBuilder
         {
             Host = databaseUri.Host,
@@ -47,8 +47,9 @@ public static class DataUtility
             Username = userInfo[0],
             Password = userInfo[1],
             Database = database,
-            SslMode = SslMode.Prefer,
+            SslMode = SslMode.Prefer
         };
+
         return builder.ToString();
     }
 
